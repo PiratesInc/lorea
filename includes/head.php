39580a22dacb7a3ca8f296203ae837505563c444
Nicolas Lundin <nlundin@gmail.com>
2013-04-05 12:54:09 -0300

    flexslider fix
    probar arriba, en local a veces no me carga bien la wea!!! :/

--- includes/head.php
+++ includes/head.php
@@ -8,43 +8,41 @@
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"/>
 
 
-        <link rel="stylesheet" href="./css/bootstrap.min.css">
-        <link rel="stylesheet" href="./css/flexslider.css">
-        <link rel="stylesheet" href="./css/main.css">
+        <link rel="stylesheet" href="../css/bootstrap.min.css">
+        <link rel="stylesheet" href="../css/flexslider.css">
+        <link rel="stylesheet" href="../css/main.css">
         
 
-        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-        <script src="./js/vendor/bootstrap.js"></script>
-        <script src="./js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
-        <script src="http://flexslider.woothemes.com/js/jquery.flexslider.js"></script>
-		<script type="text/javascript" charset="utf-8">
-		    $(function(){
-		      SyntaxHighlighter.all();
-		    });
-		    $(window).load(function(){
-		      $('#carousel').flexslider({
-		        animation: "slide",
-		        controlNav: false,
-		        animationLoop: false,
-		        slideshow: false,
-		        itemWidth: 100,
-		        itemMargin: 5,
-		        asNavFor: '#slider'
-		      });
-		      
-		      $('#slider').flexslider({
-		        animation: "slide",
-		        controlNav: false,
-		        animationLoop: false,
-		        slideshow: false,
-		        sync: "#carousel",
-		        start: function(slider){
-		          $('body').removeClass('loading');
-		        }
-		      });
-		    });
-  		</script>
+        <script src="../js/vendor/jquery-1.9.1.min.js"></script>
+        <script src="../js/vendor/bootstrap.js"></script>
+        <!-- FlexSlider -->
+		<script src="../js/vendor/jquery.flexslider.js"></script>
+        <script src="../js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
+		
+		
 
+		<script type="text/javascript">
+			$(window).load(function() {
+			// The slider being synced must be initialized first
+				$('#carousel').flexslider({
+				animation: "slide",
+				controlNav: false,
+				animationLoop: false,
+				slideshow: false,
+				itemWidth: 233,
+				itemMargin: 5,
+				asNavFor: '#slider'
+				});
+
+				$('#slider').flexslider({
+				animation: "slide",
+				controlNav: false,
+				animationLoop: false,
+				slideshow: false,
+				sync: "#carousel"
+				});
+			});
+		</script>
 
     </head>
     <body>
--- videos.php
+++ videos.php
@@ -1,36 +1,41 @@
 
-
-
+<!-- Place somewhere in the <body> of your page -->
 <div id="slider" class="flexslider">
   <ul class="slides">
     <li>
-					<iframe width="640" height="360" src="http://www.youtube.com/embed/8c_9-EbNlFI?rel=0" frameborder="0" allowfullscreen></iframe>				
+      <iframe id="player_1" src="http://player.vimeo.com/video/39683393?api=1&player_id=player_1" width="500" height="280" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
     </li>
     <li>
-					<iframe width="640" height="360" src="http://www.youtube.com/embed/8c_9-EbNlFI?rel=0" frameborder="0" allowfullscreen></iframe>				
+      <iframe id="player_1" src="http://player.vimeo.com/video/39683393?api=1&player_id=player_1" width="500" height="280" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
     </li>
     <li>
-					<iframe width="640" height="360" src="http://www.youtube.com/embed/8c_9-EbNlFI?rel=0" frameborder="0" allowfullscreen></iframe>				
+      <iframe id="player_1" src="http://player.vimeo.com/video/39683393?api=1&player_id=player_1" width="500" height="280" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
     </li>
     <li>
-					<iframe width="640" height="360" src="http://www.youtube.com/embed/8c_9-EbNlFI?rel=0" frameborder="0" allowfullscreen></iframe>				
+      <iframe id="player_1" src="http://player.vimeo.com/video/39683393?api=1&player_id=player_1" width="500" height="280" frameborder="0" webkitAllowFullScreen mozallowfullscreen allowFullScreen></iframe>
     </li>
+    <!-- items mirrored twice, total of 12 -->
   </ul>
 </div>
-
 <div id="carousel" class="flexslider">
-  <ul class="slides">
-    <li>
-	      <a>LINK 1</a>
-    </li>
-    <li>
-	      <a>LINK 2</a>
-    </li>
-    <li>
-	      <a>LINK 3</a>
-    </li>
-    <li>
-	      <a>LINK 4</a>
-    </li>
-  </ul>
+    <ul class="slides">
+        <li class="access">
+            <a href="#">
+                <h2>ACCESS</h2>
+                <p>Search all scientific research articles and journals.. if it's on Open access, it's on rock your paper.</p>
+            </a>
+        </li>
+        <li class="collaborate">
+            <a href="#">
+                <h2>COLLABORATE</h2>
+                <p>Meet your peers, review, discuss or rate an article of your interest.</p>
+            </a>
+        </li>
+        <li class="share flex-active-slide">
+            <a href="#">
+                <h2>SHARE</h2>
+                <p>Publish your research work with us. Get a strong peer review and a great visibility.</p>
+            </a>
+        </li>
+    </ul>
 </div>
